07e770f0f6195722b9032f15d14c5c574a6ec919
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-12-20 22:13:30 +0900

    update model/serializer: Actor, Movie

--- movies/models.py
+++ movies/models.py
@@ -20,7 +20,7 @@ class Movie(models.Model):
                               on_delete=models.CASCADE,
                               null=True)
     genre = models.CharField(max_length=100)
-    review = models.CharField(max_length=10, null=True) # foreign key review
+    # review = models.CharField(max_length=10, null=True) # foreign key review
     released_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- movies/serializer.py
+++ movies/serializer.py
@@ -4,32 +4,32 @@ GENRE_CHOICES = [('0', 'ACTION'), ('1', 'ROMANCE'), ('2', 'SF'), ('3', 'THRILLER
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ['id', 'title', 'content', 'director', 'actor', 'genre', 'review', 'released_at']
-
     actor = serializers.ReadOnlyField(source='actor.name')
     review = serializers.SerializerMethodField()
     genre = serializers.ChoiceField(choices=GENRE_CHOICES)
 
-    def get_review(self, obj):
-        review = obj.reviews.filter()
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'content', 'director', 'actor', 'genre', 'review', 'released_at']
+
+    def get_review(self, instance):
+        review = instance.reviews.filter(movie=instance.id)
         serializer = ReviewSerializer(review, many=True)
         return serializer.data
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title'. instance.title)
-        instance.content = validated_data.get('content'. instance.content)
-        instance.director = validated_data.get('director'. instance.director)
-        instance.actor = validated_data.get('actor'. instance.actor)
-        instance.genre = validated_data.get('genre'. instance.genre)
-        instance.review = validated_data.get('review', instance.review)
-        instance.released_at = validated_data.get('released_at', instance.released_at)
-        instance.save()
-        return instance
+    #
+    # def create(self, validated_data):
+    #     return Movie.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title'. instance.title)
+    #     instance.content = validated_data.get('content'. instance.content)
+    #     instance.director = validated_data.get('director'. instance.director)
+    #     instance.actor = validated_data.get('actor'. instance.actor)
+    #     instance.genre = validated_data.get('genre'. instance.genre)
+    #     instance.review = validated_data.get('review', instance.review)
+    #     instance.released_at = validated_data.get('released_at', instance.released_at)
+    #     instance.save()
+    #     return instance
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -38,12 +38,18 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'movie', 'author', 'review', 'comment']
         read_only_fields = ['author']
 
-    def create(self, validated_data):
-        return Review.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Review.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.movie = validated_data.get('movie'.instance.movie)
+    #     instance.review = validated_data.get('review', instance.review)
+    #     instance.comment = validated_data.get('comment'. instance.content)
+    #     instance.save()
+    #     return instance
 
-    def update(self, instance, validated_data):
-        instance.movie = validated_data.get('movie'.instance.movie)
-        instance.review = validated_data.get('review', instance.review)
-        instance.comment = validated_data.get('comment'. instance.content)
-        instance.save()
-        return instance
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['id', 'name', 'filmography']
