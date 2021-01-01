583022e04d78a3dbbd4891ada4d9893baf0558dd
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2021-01-01 22:33:20 +0900

    Update deploy.py

--- MovieLiker/settings/deploy.py
+++ MovieLiker/settings/deploy.py
@@ -1,34 +1,26 @@
 from .base import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7u%im-drc*n6i#3m#icvjk(qd$5&0ea44b0r&6k=8%sp3#xb$d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["158.247.221.120"]
-
 
 def read_secret(secret_name):
     file = open('/run/secrets/' + secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
-
     return secret
 
 
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+DEBUG = False
+ALLOWED_HOSTS = ["158.247.221.120"]
+
+
 # Database: Mariadb
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'PASSWORD': read_secret('MYSQL_ROOT_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
